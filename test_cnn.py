05125f6721af1823ecd1b343f0bc5fcb53515fc3
Rivalcoba <ivan.rivalcoba@gmail.com>
2018-02-07 18:06:10 +0100

    Testing bulk images ready

--- test_cnn.py
+++ test_cnn.py
@@ -1,15 +1,19 @@
-# python -i catdogset/single_prediction/cat0.jpg
+# python test_cnn.py -t catdogset/single_prediction
+
+# Importing dependencies
 import cv2
+from imutils import paths
 from keras.models import model_from_json
 from keras.optimizers import SGD
 from keras.preprocessing import image
 import numpy as np
 import scipy.misc
-
 import argparse
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=False, help="path to the image to being classified")
+ap.add_argument("-t", "--test", required=True, help="folder containing the test iamges")
+args = vars(ap.parse_args())
 
 # Loading Model
 model_architecture = "catdog_architecture.json"
@@ -17,15 +21,19 @@ model_weights = "catdog_architecture.h5"
 classifier = model_from_json(open(model_architecture).read())
 classifier.load_weights(model_weights)
 
+# Compiling classifier
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-test_image = image.load_img("catdogset/single_prediction/cat0.jpg", target_size = (64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
+# Grab the Training image paths
+imageTrainPaths = list(paths.list_images(args['test']))
 
-if result[0][0] == 1:
-    prediction = 'dog'
-else:
-    prediction = 'cat'
-print("Prediction {0}".format(prediction))
+for imagePath in imageTrainPaths:
+    test_image = image.load_img(imagePath, target_size = (64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = classifier.predict(test_image)
+    if result[0][0] == 1:
+        prediction = 'dog'
+    else:
+        prediction = 'cat'
+    print("File: {0} \n Prediction {1}".format( imagePath.split('\\')[1], prediction))
